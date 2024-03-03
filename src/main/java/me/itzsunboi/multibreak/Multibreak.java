164fa6b9be6ac47324d824851c234ccab5a38119
BuildTools <unconfigured@null.spigotmc.org>
2024-03-03 20:05:41 +0000

    Initial commit

---
+++ src/main/java/me/itzsunboi/multibreak/Multibreak.java
@@ -0,0 +1,17 @@
+package me.itzsunboi.multibreak;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Multibreak extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
